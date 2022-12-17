17f101d7ab48540ceff504b6a48636278e297c83
EtienneNoir <etiennebanza37@gmail.com>
2022-12-17 21:32:52 +0200

    Populating the category table

--- index.php
+++ index.php
@@ -5,11 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
 </head>
 <body>
     <?php
         include 'config.php'; // importing config page, to use its properties
         $connect = OpenConnection(); // calling the function to make a connection to the databse
+
+        
+
+
+        $query = "INSERT INTO Category (Category_Name, Category_Image, Category_Description) 
+        VALUES ('Laptops', 'Images/Lap.jpg', 'Search Laptops')";
+
+        $results = mysqli_query($connect, $querys) or die("Unable to retrieve data!");// Execute query using specified connection 
+    
+    
     ?>
 </body>
 </html>
