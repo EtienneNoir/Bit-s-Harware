d967cd16d6dc05a3ee3641bdadbde4f57fbb3805
EtienneNoir <etiennebanza37@gmail.com>
2022-12-20 16:05:32 +0200

    Now to show the details of the second row

--- ShowProducts.php
+++ ShowProducts.php
@@ -143,7 +143,7 @@
                         
                     echo "<td>";
                     echo "<a href=\"$href\">";
-                    echo "<img src=\"$image\"  title=\"$description\" alt=\"$alt\" width=\"450\" height=\"380\" onclick=\"myFunction('$image' , '$price' , '$description')\">";
+                    echo "<img src=\"$image\"  title=\"$description\" alt=\"$alt\" width=\"450\" height=\"380\">";
                     echo "</a>";
                     echo "</td>";
 
