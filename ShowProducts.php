78c45ac19e4e4eb016e4d842dd3cf37879307a57
EtienneNoir <etiennebanza37@gmail.com>
2022-12-20 12:10:16 +0200

    Trying to print out the description of the product

--- ShowProducts.php
+++ ShowProducts.php
@@ -113,7 +113,7 @@
                         
                     echo "<td>";
                     echo "<a href=\"$href\">";
-                    echo "<img src=\"$image\" alt=\"$alt\" width=\"450\" height=\"380\" onclick=\"myFunction('$image' , '$description')\">";
+                    echo "<img src=\"$image\" alt=\"$alt\" width=\"450\" height=\"380\" onclick=\"myFunction('$image')\">";
                     echo "</a>";
                     echo "</td>";
 
@@ -236,12 +236,12 @@
        
 
 
-    function myFunction(image , text){
+    function myFunction(image ){
 
         document.getElementById("popMessage").style.display = "block";
         document.getElementById("backdrop").style.display = "block";   
         document.getElementById("images").src = image;
-        document.getElementById("about").innerHTML = text;
+    
 
 
     }
