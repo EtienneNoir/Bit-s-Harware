7a1c6b9d966d3d345ec4ed894b742d9e38075aa2
EtienneNoir <etiennebanza37@gmail.com>
2022-12-23 23:02:12 +0200

    Added a password validation in the Profile.html and retrieving data once a button is selected

--- MidPoint.php
+++ MidPoint.php
@@ -7,12 +7,26 @@
         include 'config.php'; // importing config page, to use its properties
 
         $connect = OpenConnection(); // calling the function to connect to the database and storing its return value
-    
+ 
         if(isset($_REQUEST["Register"])){
 
             $name = $_POST['Unam'];
-            
-            echo $name;
+            $name = $_POST['Lanam'];
+            $name = $_POST['E'];
+            $name = $_POST['T'];
+            $name = $_POST['Address'];
+            $name = $_POST['p'];
+
+            $query1 = "SELECT * FROM Customers WHERE"; 
+
+            $result = mysqli_query($connect, $query1) or die("Unable to connect to database!"); // Execute query then return the result
+
+            $Allrecords = mysqli_num_rows($result); // is used to return the number of rows returned from the data base based on the query
+
+        if ($Allrecords == 0) {
+
+            echo "Nothing to check current credentials with, this is indicating that this is the first customer";
+        }
 
         }
         else{
