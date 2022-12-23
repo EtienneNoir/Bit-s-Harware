9764546f8b4ad73611541d52f18857bbb0ff7288
EtienneNoir <etiennebanza37@gmail.com>
2022-12-23 21:01:09 +0200

    Created a page to verify user

---
+++ MidPoint.php
@@ -0,0 +1,15 @@
+<?php 
+
+    /* 
+    
+        * This page verifies the User's Log in , successful log in will create a session to all the subscribed pages */
+        
+        include 'config.php'; // importing config page, to use its properties
+
+        $connect = OpenConnection(); // calling the function to connect to the database and storing its return value
+    
+        
+    
+
+
+?>
