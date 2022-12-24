bd0b0d3749a2205c64aa7bdce3634fe95f28665d
EtienneNoir <etiennebanza37@gmail.com>
2022-12-24 06:50:38 +0200

    Fixed the style of the Midpoint page

--- MidPoint.php
+++ MidPoint.php
@@ -80,56 +80,55 @@
             } else { // Now to make sure to let the customer know if their credentials are already in the database 
 
 
-            $carry = ""; // Will hold the duplicate value 
-
+           
                 // Then take results obtained from the initial query, go through every records and find any values in the database that is identical to the user's credentials
                 while ($record = mysqli_fetch_array($result)) { 
 
                         if ($email == $record['Customer_email']) {
 
-                            $carry = "Email already contained in database, please try again";
-                            
-                    echo "div id=\"main2\" >
-                        <form action=\"MidPoint.php\" id=\"fomSign\" name=\"newAccountform\" method=\"post\"> 
-                            <table> <!-- Used to make sure that all the content are aligned -->
-                            <h2 style=\"font-family: Monospace font-size=large\"><img src=\"Images/248961.png\" alt=\"Image of gear\" id=\"load2\" class=\"Icons\"> Register </h2>
-                            <div>
-                                <tr><!-- First row-->
-                                <td><input type=\"text\" class=\"field\" id=\"Uname\" name=\"Unam\"placeholder=\"First Name\" autofocus required></td>
-                                </tr>
+                            echo '<script>alert("Email already contained in database, please try again")</script>';
+
+                            echo "div id=\"main2\" >
+                                <form action=\"MidPoint.php\" id=\"fomSign\" name=\"newAccountform\" method=\"post\"> 
+                                    <table> <!-- Used to make sure that all the content are aligned -->
+                                    <h2 style=\"font-family: Monospace font-size=large\"><img src=\"Images/248961.png\" alt=\"Image of gear\" id=\"load2\" class=\"Icons\"> Register </h2>
+                                    <div>
+                                        <tr><!-- First row-->
+                                        <td><input type=\"text\" class=\"field\" id=\"Uname\" name=\"Unam\"placeholder=\"First Name\" value=\"$name\" autofocus required></td>
+                                        </tr>
+                                        
+                                        <img src=\"Images/user.png\" alt=\"Image of User\"  class=\"Icons\"> 
+                                    </div>
+
+                                    <tr>
+                                    <td><input type=\"text\" class=\"field\" id=\"Laname\" name=\"Lanam\" class=\"Icons1\" placeholder=\"Last Name\" value=\"$Lname\" required></td>
+                                    </tr>
+
+                                    <tr>
+                                    <td><input type=\"email\" class=\"field\" id=\"Email\" name=\"E\" class=\"Icon1s\" value=\"$email\"placeholder=\"Email\" required></td>
+                                    </tr>
+
+                                    <tr>
+                                    <td><input type=\"tel\" class=\"field\" id=\"Tele\" name=\"T\" class=\"Icon1s\" value=\"$phone\" placeholder=\"Phone Number\" required></td>
+                                    </tr>
+
+                                    <tr>
+                                    <td><input type=\"text\" class=\"field\" id=\"Add\" name=\"Address\" class=\"Icons1\" value=\"$address\" placeholder=\"Address\" required></td>
+                                    </tr>
+
+                                    <tr>
+                                    <td> <input type=\"password\" class=\"field\" id=\"ps\" name=\"p\" class=\"Icons1\" value=\"$pass\" placeholder=\"Password\" required pattern=\"^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,50}$\" title=\"Your Password must have at least one number and one uppercase and lowercase letter and one special character , and at least 8 or more characters\"> </td>
+                                    </tr>
+                                    
+                                    <tr>
+                                    <td> <input type=\"submit\" value=\"Create Account\" id=\"SignUp\" name=\"Register\"> </td>
+                                    </tr>
+                                    </table>
+                                    
+                                    <span> Already Have an Account? </span> <a class=\"button\" id=\"Log\" style=\"color: #fc8129;\" onclick=\"LogIn()\">Log In</a>
+                                </form>
+                                </div> </body>";
                                 
-                                <img src=\"Images/user.png\" alt=\"Image of User\"  class=\"Icons\"> 
-                            </div>
-
-                            <tr>
-                            <td><input type=\"text\" class=\"field\" id=\"Laname\" name=\"Lanam\" class=\"Icons1\" placeholder=\"Last Name\" required></td>
-                            </tr>
-
-                            <tr>
-                            <td><input type=\"email\" class=\"field\" id=\"Email\" name=\"E\" class=\"Icon1s\" placeholder=\"Email\" required></td>
-                            </tr>
-
-                            <tr>
-                            <td><input type=\"tel\" class=\"field\" id=\"Tele\" name=\"T\" class=\"Icon1s\" placeholder=\"Phone Number\" required></td>
-                            </tr>
-
-                            <tr>
-                            <td><input type=\"text\" class=\"field\" id=\"Add\" name=\"Address\" class=\"Icons1\" placeholder=\"Address\" required></td>
-                            </tr>
-
-                            <tr>
-                            <td> <input type=\"password\" class=\"field\" id=\"ps\" name=\"p\" class=\"Icons1\" placeholder=\"Password\" required pattern=\"^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W]){1,})(?!.*\s).{8,50}$\" title=\"Your Password must have at least one number and one uppercase and lowercase letter and one special character , and at least 8 or more characters\"> </td>
-                            </tr>
-                            
-                            <tr>
-                            <td> <input type=\"submit\" value=\"Create Account\" id=\"SignUp\" name=\"Register\"> </td>
-                            </tr>
-                            </table>
-                            
-                            <span> Already Have an Account? </span> <a class=\"button\" id=\"Log\" style=\"color: #fc8129;\" onclick=\"LogIn()\">Log In</a>
-                        </form>
-                        </div>";
-      
    
                             break;
                 
