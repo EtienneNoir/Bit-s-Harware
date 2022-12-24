053d3bfa2f25b552a614c5b5ecbc8f06cf29dedc
EtienneNoir <etiennebanza37@gmail.com>
2022-12-24 09:16:55 +0200

    Increased the height of the Log in Form when submitted in the form div and not the outer div

--- MidPoint.php
+++ MidPoint.php
@@ -220,8 +220,8 @@
 
             if ($Allrecords == 0) { //an implication that the user passed on the wrong credentials or the user is simply not in the database
 
-                echo "<div id=\"LogIn\" style=\"display:block; height: 460px\"  >
-                    <form action=\"MidPoint.php\" id=\"fomSign2\" name=\"OldAccount\" method=\"post\"> <!-- Post method, used to post things on the database -->
+                echo "<div id=\"LogIn\" style=\"display:block;\"  >
+                    <form style=\"height: 460px\" action=\"MidPoint.php\" id=\"fomSign2\" name=\"OldAccount\" method=\"post\"> <!-- Post method, used to post things on the database -->
                     <table> <!-- Used to make sure that all the content are aligned -->
                         <h2 style=\"font-family: Monospace font-size=large\"><img src=\"Images/248961.png\" alt=\"Image of gear\" id=\"load\" class=\"Icons\" > Log In </h2>
                         <div>
