732e683211e1d0afbd0542be855cbd45478532d2
EtienneNoir <etiennebanza37@gmail.com>
2022-12-25 16:37:44 +0200

    Fixed the initial query in Edit.php

--- Subscribed/Edit.php
+++ Subscribed/Edit.php
@@ -61,9 +61,9 @@ session_start();
 
         // create a main query that will take the values from the database 
 
-        $MainQuery = "SELECT * FROM customers WHERE Customer_email = '$email'"; 
+        $MainQuery = "SELECT * FROM Customers WHERE Customer_email = '$email'"; 
 
-        $Mainresult = mysqli_query($connect, $Mainquery) or die("Unable to connect to database!W"); // The result is then returned
+        $Mainresult = mysqli_query($connect, $Mainquery) or die("Unable to connect to database!"); // The result is then returned
 
         /* These new values will be passed on to the updated form instead of making another query to retrieve something that we already have */
 
