80f099320f367e8930adf36007abfea5f96e0bd6
EtienneNoir <etiennebanza37@gmail.com>
2022-12-29 22:01:05 +0200

    Added a different cart image

--- Subscribed/ShowProducts2.php
+++ Subscribed/ShowProducts2.php
@@ -80,7 +80,7 @@ session_start();
                 </li>
 
                 <li>
-                    <a href=\"javascript:void(0)\" ><img src=\"../Images/pngwing.com.png\" alt=\"Image of Cart\"  class=\"Icons\"> $quantity </div> </a> 
+                    <a href=\"javascript:void(0)\" ><img src=\"../Images/pngwing.com.png\" alt=\"Image of Cart\"> $quantity </div> </a> 
                 </li>
 
                 <li>
