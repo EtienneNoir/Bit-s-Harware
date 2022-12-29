81e69b922ac3ff3e79ab36ce53eaf59ccf9739fe
EtienneNoir <etiennebanza37@gmail.com>
2022-12-29 22:07:01 +0200

    Changed the size of the cart

--- Subscribed/ShowProducts2.php
+++ Subscribed/ShowProducts2.php
@@ -80,7 +80,7 @@ session_start();
                 </li>
 
                 <li>
-                    <a href=\"javascript:void(0)\" ><img src=\"../Images/pngwing.com.png\" alt=\"Image of Cart\"> $quantity </div> </a> 
+                    <a href=\"javascript:void(0)\" ><img src=\"../Images/pngwing.com.png\" alt=\"Image of Cart\" id=\"cartPng\"> $quantity </div> </a> 
                 </li>
 
                 <li>
