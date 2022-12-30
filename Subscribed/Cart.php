da5a26e11f7dcad89eb78bb148afdffead9df82b
EtienneNoir <etiennebanza37@gmail.com>
2022-12-30 15:24:42 +0200

    Added another isset function for the purchase button

--- Subscribed/Cart.php
+++ Subscribed/Cart.php
@@ -59,99 +59,105 @@ session_start();
 
     $connect = OpenConnection(); // calling the function to connect to the database and storing its return value
 
-    if (isset($_REQUEST["remove"])) {
+    if(isset($_REQUEST['Cart'])){
 
-        $Item_id = $_REQUEST['id'];
+        echo "Submitted";
 
-        $delQuery = "DELETE FROM Cart_Item WHERE Cart_id = '$Item_id'";
+    } else {
+        if (isset($_REQUEST["remove"])) {
 
-        $delResult = mysqli_query($connect, $delQuery) or die("Unable to connect to database!"); // Execute query then return the result
-        
-        $_SESSION['Quantity'] = $_SESSION['Quantity'] - 1;// To visually show that an item has been removed
+            $Item_id = $_REQUEST['id'];
 
-        echo '<script>alert("Item has been Removed from cart")</script>';
+            $delQuery = "DELETE FROM Cart_Item WHERE Cart_id = '$Item_id'";
+
+            $delResult = mysqli_query($connect, $delQuery) or die("Unable to connect to database!"); // Execute query then return the result
+    
+            $_SESSION['Quantity'] = $_SESSION['Quantity'] - 1; // To visually show that an item has been removed
+    
+            echo '<script>alert("Item has been Removed from cart")</script>';
+
+
+        }
+        $IdQuery = "SELECT * FROM Cart_Item WHERE Customer_id = '$Customer_id'";
+
+        $idResult = mysqli_query($connect, $IdQuery) or die("Unable to connect to database!"); // Execute query then return the result
     
 
-    }
-    $IdQuery = "SELECT * FROM Cart_Item WHERE Customer_id = '$Customer_id'";
+        echo "<ul id=\"items\">";
+        while ($recordsid = mysqli_fetch_array($idResult)) {
 
-    $idResult = mysqli_query($connect, $IdQuery) or die("Unable to connect to database!"); // Execute query then return the result
+            $Product_id = $recordsid['Product_id'];
 
+            $Cart_id = $recordsid['Cart_id'];
 
-    echo "<ul id=\"items\">";
-    while ($recordsid = mysqli_fetch_array($idResult)){
+            $Product_Query = "SELECT * FROM Product WHERE Product_id = '$Product_id'";
 
-        $Product_id = $recordsid['Product_id'];
-
-        $Cart_id =  $recordsid['Cart_id'];
-
-        $Product_Query = "SELECT * FROM Product WHERE Product_id = '$Product_id'";
-
-        $Product_Result = mysqli_query($connect, $Product_Query) or die("Unable to connect to database!"); // Execute query then return the result
-
-        $Product_Info = mysqli_fetch_array($Product_Result);
-
-        $image = $Product_Info["Product_Image"];
-        $about = $Product_Info["description"];
-        $price = $Product_Info['Price'];
-        $name = $Product_Info['Product_Name'];
+            $Product_Result = mysqli_query($connect, $Product_Query) or die("Unable to connect to database!"); // Execute query then return the result
     
-        echo "<li>";
-        
-        
-        
-        echo"<div id=\"Cart\">
-        
+            $Product_Info = mysqli_fetch_array($Product_Result);
+
+            $image = $Product_Info["Product_Image"];
+            $about = $Product_Info["description"];
+            $price = $Product_Info['Price'];
+            $name = $Product_Info['Product_Name'];
+
+            echo "<li>";
 
 
-        
 
-        <div id=\"popMessage\"> 
-            <form action=\"Cart.php?id=$Cart_id\" method=\"post\">
-                <input type=\"submit\" id=\"remove\" value=\"Remove Item\" name=\"remove\"> 
-                <input type=\"hidden\" name=\"P_id\" value=\"$name\" id=\"ids\">
-            </form>
+            echo "<div id=\"Cart\">
             
-            <div id=\"image\"> 
-
-                <img id=\"images\" src=\"../$image\" alt=\"\"  width=\"450\" height=\"380\" title=\"$name\"/>
-
-            </div>
 
 
-            <div id=\"content\">
+            
 
-                <h3 id=\"h3\"> </h3> 
-
-                <h4> Key Features: </h4>
-                <p id=\"about\">
-                    $about
-                </p>
-
+            <div id=\"popMessage\"> 
+                <form action=\"Cart.php?id=$Cart_id\" method=\"post\">
+                    <input type=\"submit\" id=\"remove\" value=\"Remove Item\" name=\"remove\"> 
+                    <input type=\"hidden\" name=\"P_id\" value=\"$name\" id=\"ids\">
+                </form>
                 
-                <h4 id=\"price\"> R $price </h4>
+                <div id=\"image\"> 
 
-            </div>
+                    <img id=\"images\" src=\"../$image\" alt=\"\"  width=\"450\" height=\"380\" title=\"$name\"/>
+
+                </div>
 
 
-        
+                <div id=\"content\">
+
+                    <h3 id=\"h3\"> </h3> 
+
+                    <h4> Key Features: </h4>
+                    <p id=\"about\">
+                        $about
+                    </p>
+
+                    
+                    <h4 id=\"price\"> R $price </h4>
+
+                </div>
 
 
-        </div>";
+            
 
-        echo "</li>";
+
+            </div>";
+
+            echo "</li>";
+        }
+        echo "</ul><br>";
+
+        echo "</div><br>";
+
+
+
+        CloseConnection($connect); // Closing the connection 
+    
+        echo "<form action=\"Cart.php\" method=\"post\" id=\"Purchase_form\" >
+                <input type=\"submit\" value=\"Purchase order\" id=\"buy\" name=\"Cart\">
+            </form>";
     }
-    echo "</ul><br>";
-
-    echo "</div><br>";
-
-   
-
-    CloseConnection($connect); // Closing the connection 
-
-    echo"<form action=\"Order.php\" method=\"post\" id=\"Purchase_form\" >
-            <input type=\"submit\" value=\"Purchase order\" id=\"buy\" name=\"Cart\">
-        </form>";
        
     echo "<br><br>"
     
---
+++ Subscribed/Order.php
@@ -0,0 +1,6 @@
+- The cart page should also have another if isset when the purchase order is submitted
+
+- Once the purchase button has been selected, the remaining items in the cart should be be placed in an order table #818181
+
+- A return with a check mark to indicate that the purchase has been made to the user
+
